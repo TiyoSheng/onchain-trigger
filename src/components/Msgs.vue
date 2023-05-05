b835c3eab4d4677c961fa886a27195dbd2ef42f3
Tiyo <tiyosheng@gmail.com>
2023-05-06 01:17:11 +0800

    fixbug

--- src/components/Msgs.vue
+++ src/components/Msgs.vue
@@ -352,6 +352,7 @@ const applyFun = async (list, paramList, time) => {
         data: swapQuoteJSON.data,
         value: swapQuoteJSON.value,
         gasPrice: swapQuoteJSON.gasPrice,
+        gasLimit: swapQuoteJSON.gasLimit
       }
       const receipt = await wallet.sendTransaction(data)
       receipt.wait()
